95920021bc2bcc96fa7336cd2c38ae592a28308e
noah-bar <noah.barberini@eduvaud.ch>
2024-02-27 14:42:32 +0100

    feat: add type to input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -3,13 +3,14 @@ import { twMerge } from "tailwind-merge"
 import { v4 as randomUUID } from 'uuid'
 
 type InputProps = HTMLAttributes<HTMLInputElement> & {
-    label?: string
+    label?: string,
+    type?: "text" | "number" | "file"
 }
-export default function Input({children, label, className, ...props}: InputProps) {
+export default function Input({children, label, className, type="text", ...props}: InputProps) {
     const style = "flex flex-col w-[185px]"
     const inputId = useRef(randomUUID())
     return <div className={twMerge(style, className)}>
         {(label || children) && <label htmlFor={inputId.current}>{label ? label : children}</label>}
-        <input {...props} id={inputId.current} className="bg-transparent border-b border-b-gray-500 outline-none"/>
+        <input {...props} id={inputId.current} type={type} className="bg-transparent border-b border-b-gray-500 outline-none"/>
     </div>
 }
