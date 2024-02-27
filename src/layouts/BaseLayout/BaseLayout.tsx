4f81c753edb6e45b80a6f4ad76e33d5fe31e771e
noah-bar <noah.barberini@eduvaud.ch>
2024-02-27 14:11:12 +0100

    feat: add lang select

--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,11 +1,17 @@
 import {PropsWithChildren} from "react";
+import Select from "../../components/ui/Select";
 
 type BaseLayoutProps = PropsWithChildren
 export default function BaseLayout({children}: BaseLayoutProps) {
     return (
         <>
-            <header className="w-full h-16 bg-zinc-950 border-b border-b-gray-500">
-
+            <header className="flex justify-between items-center w-full h-16 bg-zinc-950 border-b border-b-gray-500 p-4">
+                <span className="text-xl">RIA2</span>
+                <Select>
+                    <option value="fr">FR</option>
+                    <option value="en">EN</option>
+                    <option value="de">DE</option>
+                </Select>
             </header>
             <main className={"h-full container mx-auto"}>
                 {children}
