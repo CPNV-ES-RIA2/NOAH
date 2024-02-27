fcd6222cf04a60f7ed910f69158087bf4ef3b486
noah-bar <noah.barberini@eduvaud.ch>
2024-02-27 18:13:06 +0100

    feat: add label detector form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import LabelDetectorForm from "./forms/LabelDetectorForm.tsx";
 import BaseLayout from "./layouts/BaseLayout/BaseLayout.tsx";
+import { LabelDetectorFormType } from "./forms/LabelDetectorForm.tsx";
 
 export default function App() {
+    const handleLabelDetectorFormSubmit = (data: LabelDetectorFormType) => {
+        console.log(data)
+    }
+
     return (
         <BaseLayout>
-            <h1 className="text-3xl">Home</h1>
+            <h1 className="m-4 text-center text-xl">Label detector</h1>
+            <LabelDetectorForm onSubmit={handleLabelDetectorFormSubmit}/>
         </BaseLayout>
     )
 }
