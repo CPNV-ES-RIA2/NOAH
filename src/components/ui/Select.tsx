c03d849a29a00d7ae9d270de76b48cb24798fb99
noah-bar <noah.barberini@eduvaud.ch>
2024-02-27 14:21:55 +0100

    style: change bg color

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge"
 
 type SelectProps = HTMLAttributes<HTMLSelectElement>
 export default function Select({children,className, ...props}: SelectProps) {
-    const style = "bg-zinc-950 p-2 border-b border-b-gray-500"
+    const style = "bg-transparent p-2 border-b border-b-gray-500"
     return <select {...props} className={twMerge(style, className)}>
         {children}
     </select>
