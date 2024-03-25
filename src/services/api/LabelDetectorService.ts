7442b3c8856c00e9a8f2cc07d69ce72c5c64a156
= <=>
2024-03-25 18:45:52 +0100

    feat: add label detector service

--- src/services/api/LabelDetectorService.ts
+++ src/services/api/LabelDetectorService.ts
@@ -9,16 +9,16 @@ type AnalyseProps = {
 
 export class LabelDetectorService extends ApiService {
 
-  public static async analyse({ image, maxResults, minConfidenceLevel }: AnalyseProps): Promise<Label[]> {
+  public static async analyse(props: AnalyseProps): Promise<Label[]> {
     const endpoint = `${this.apiUrl}/analyse`
     const response = await fetch(endpoint, {
       ...this.requestInit,
+      headers: {
+        "accept": "application/json",
+        "content-type": "application/json",
+      },
       method: "POST",
-      body: JSON.stringify({
-        image,
-        maxResults,
-        minConfidenceLevel
-      })
+      body: JSON.stringify(props)
     })
 
     if(!response.ok) await this.throwException(response)
