65810ec052b2ef9ae68565c409f21a74696ea6a8
= <=>
2024-03-26 19:14:27 +0100

    feat: add translation for Loader component

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -9,7 +9,7 @@ export default function Loader({ isLoading }: LoaderProps) {
     if(!isLoading) return null
     return (
         <div className={"w-full h-[100vh] bg-white bg-opacity-40 absolute top-0 left-0 flex justify-center items-center"}>
-            <span className={"text-3xl text-black font-bold"}>{t('Loading...')}</span>
+            <span className={"text-3xl text-black font-bold"}>{t('Loading')}...</span>
         </div>
     )
 }
